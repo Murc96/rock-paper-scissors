4b3bc86fca086e56546fd25ae672945488a116c0
vlGoku <markolucic21@outlook.de>
2023-05-22 13:53:08 +0200

    game verbessert

--- main.js
+++ main.js
@@ -13,34 +13,34 @@ function computerPlay() {
 function PlayRound() {
   let playerSelection = prompt(
     "Bitte geben Sie Schere, Stein oder Papier ein: "
-  );
+  ).toLowerCase();
   let computerSelect = computerPlay();
 
-  if (computerSelect === "Schere" && playerSelection === "Schere") {
+  if (computerSelect === "Schere" && playerSelection === "schere") {
     console.log("Es ist ein Unentschieden");
     return 0;
-  } else if (computerSelect === "Schere" && playerSelection === "Stein") {
+  } else if (computerSelect === "Schere" && playerSelection === "stein") {
     console.log("Du gewinnst! - Stein schlägt Schere");
     return 1;
-  } else if (computerSelect === "Schere" && playerSelection === "Papier") {
+  } else if (computerSelect === "Schere" && playerSelection === "papier") {
     console.log("Du verlierst! - Schere schlägt Papier");
     return 2;
-  } else if (computerSelect === "Stein" && playerSelection === "Stein") {
+  } else if (computerSelect === "Stein" && playerSelection === "stein") {
     console.log("Es ist ein Unentschieden");
     return 0;
-  } else if (computerSelect === "Stein" && playerSelection === "Schere") {
+  } else if (computerSelect === "Stein" && playerSelection === "schere") {
     console.log("Du verlierst! - Stein schlägt Schere");
     return 2;
-  } else if (computerSelect === "Stein" && playerSelection === "Papier") {
+  } else if (computerSelect === "Stein" && playerSelection === "papier") {
     console.log("Du gewinnst! - Papier schlägt Stein");
     return 1;
-  } else if (computerSelect === "Papier" && playerSelection === "Stein") {
+  } else if (computerSelect === "Papier" && playerSelection === "stein") {
     console.log("Du verlierst! - Papier schlägt Stein");
     return 2;
-  } else if (computerSelect === "Papier" && playerSelection === "Papier") {
+  } else if (computerSelect === "Papier" && playerSelection === "papier") {
     console.log("Es ist ein Unentschieden");
     return 0;
-  } else if (computerSelect === "Papier" && playerSelection === "Schere") {
+  } else if (computerSelect === "Papier" && playerSelection === "schere") {
     console.log("Du gewinnst! - Schere schlägt Papier");
     return 1;
   }
@@ -49,7 +49,7 @@ function PlayRound() {
 function game() {
   let zaehlerPlayer = 0;
   let zaehlerComputer = 0;
-  for (i = 0; i < 5; i++) {
+  while (zaehlerComputer < 5 || zaehlerPlayer < 5) {
     let ergebnis = PlayRound();
     if (ergebnis === 0) {
       console.log("Niemand kriegt ein Punkt, da es ein unentschieden ist");
